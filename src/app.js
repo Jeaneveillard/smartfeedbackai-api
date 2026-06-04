badac9e832d475dddbc08991bc054ed0a830c34e
Jean Morrely Cazeau <cazeauvendu@gmail.com>
2026-06-04 01:49:18 -0400

    fix: allow localhost + Netlify CORS origins

--- src/app.js
+++ src/app.js
@@ -9,8 +9,16 @@ require('./auth/googleOAuth');
 const app = express();
 
 app.use(helmet());
+var allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:3000',
+  'http://127.0.0.1:3000'
+].filter(Boolean);
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function(origin, cb) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) return cb(null, true);
+    cb(new Error('CORS: origin non autorisée: ' + origin));
+  },
   credentials: true
 }));
 app.use(express.json());
